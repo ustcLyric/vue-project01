277830b4c06cb1dd1915c139611b226a13d1f4a6
ustcYang <ustcyh@ustc.edu>
2024-12-13 02:05:19 +0800

    axios配置完毕

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,10 @@
 import SvgIcon from './SvgIcon/svgIndex.vue'
+import type {App, Component} from "vue";
 
-const allGloablComponent   = {SvgIcon}
+const allGloablComponent:{[key:string]:Component}   = {SvgIcon}
 // 对外暴露插件对象
 export default {
-  install: (app) => {
+  install: (app:App) => {
     // 注册项目全部的全局组件
     Object.keys(allGloablComponent).forEach(key => {
       app.component(key, allGloablComponent[key])
